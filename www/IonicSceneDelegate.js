7fef562dac1a606c0366af141d801cf13ae7d01b
vcorreaj <vcorrea@medianet.com.ve>
2025-09-17 16:45:27 -0400

    ff

--- www/IonicSceneDelegate.js
+++ www/IonicSceneDelegate.js
@@ -12,24 +12,14 @@ var IonicSceneDelegate = {
     
     getSceneState: function(successCallback, errorCallback) {
         exec(successCallback, errorCallback, 'CDVSceneDelegate', 'getSceneState', []);
-    },
-    
-    // Eventos para suscribirse a cambios de estado
-    onSceneDidBecomeActive: function(callback) {
-        document.addEventListener('resume', callback, false);
-    },
-    
-    onSceneWillResignActive: function(callback) {
-        document.addEventListener('pause', callback, false);
     }
 };
 
-// Exportar para CommonJS
+// Exportación para diferentes entornos
 if (typeof module !== 'undefined' && module.exports) {
     module.exports = IonicSceneDelegate;
 }
 
-// Exportar globalmente para navegador
 if (typeof window !== 'undefined') {
     window.IonicSceneDelegate = IonicSceneDelegate;
 }
